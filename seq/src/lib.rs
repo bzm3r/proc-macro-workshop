c701fedb2e02d6c45fc6fbf0cd9b556b437df34c
Brian Merchant <bhmerchant@gmail.com>
2023-07-06 10:29:50 -0700

    test 2 is passing

--- seq/src/lib.rs
+++ seq/src/lib.rs
@@ -1,8 +1,51 @@
-use proc_macro::TokenStream;
+use quote::quote;
+use syn::{
+    braced,
+    parse::{Parse, ParseStream},
+    parse_macro_input,
+    token::Brace,
+    ExprBlock, Ident, LitInt, Token,
+};
+
+#[derive(Debug)]
+struct Seq {
+    loop_var: Ident,
+    range_start: LitInt,
+    range_end: LitInt,
+    body: proc_macro2::TokenStream,
+}
+
+impl Parse for Seq {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        let loop_var = input.parse()?;
+        input.parse::<Token![in]>()?;
+        let range_start = input.parse()?;
+        let range_end = input.parse()?;
+        let body;
+        braced!(body in input);
+        let body = proc_macro2::TokenStream::parse(&body)?;
+
+        eprintln!("seq");
+        Ok(Seq {
+            loop_var,
+            range_start,
+            range_end,
+            body,
+        })
+    }
+}
 
 #[proc_macro]
-pub fn seq(input: TokenStream) -> TokenStream {
-    let _ = input;
+pub fn seq(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let seq = parse_macro_input!(input as Seq);
+    // let Seq {
+    //     loop_var,
+    //     range_start,
+    //     range_end,
+    // }
 
-    unimplemented!()
+    let tokens = quote! {};
+
+    eprintln!("{:#?}", seq);
+    tokens.into()
 }
