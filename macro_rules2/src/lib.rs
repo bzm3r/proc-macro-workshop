f06511e4f6f7ba1cba32206e96bc195b57d0bac4
Brian Merchant <bhmerchant@gmail.com>
2023-07-11 17:23:07 -0700

    WIP

--- macro_rules2/src/lib.rs
+++ macro_rules2/src/lib.rs
@@ -1,45 +1,63 @@
+mod delimiter;
+mod group;
+mod macro_match;
+mod match_rep;
+mod matcher;
+mod meta_var;
+mod repetition;
+mod rule;
+mod rules_def;
+mod transcriber;
+
+use paste::paste;
 use proc_macro2::{
-    extra::DelimSpan as DelimSpan2, Delimiter as Delimiter2, TokenStream as TokenStream2,
+    extra::DelimSpan as DelimSpan2, Delimiter as Delimiter2, Spacing as Spacing2,
+    TokenStream as TokenStream2, TokenTree as TokenTree2,
 };
-use std::fmt::Debug;
+use proc_macro2::{Group, Punct, Span};
+use quote::{quote, ToTokens};
+use std::fmt::{Display, Error as FmtError, Formatter, Result as FmtResult};
+use std::iter::once;
+use std::ops::{Deref, DerefMut};
+use std::{fmt::Debug, marker::PhantomData, ops::Range};
 use syn::{
-    parse::{discouraged::AnyDelimiter, Parse, ParseStream, Result as ParseResult},
-    parse_macro_input, Ident,
+    buffer::Cursor,
+    parse::{
+        discouraged::AnyDelimiter, Error as ParseError, Parse, ParseBuffer, ParseStream,
+        Result as ParseResult,
+    },
+    parse_macro_input,
+    token::{Brace as SynBrace, Bracket as SynBracket, Paren as SynParen, SelfType, Token},
+    Ident, ItemMacro, MacroDelimiter, Token,
 };
 
-#[derive(Debug)]
-struct Delimited {
-    delimiter: Delimiter2,
-    span: DelimSpan2,
-    contents: TokenStream2,
-}
-
-impl Parse for Delimited {
-    fn parse(input: ParseStream<'_>) -> ParseResult<Self> {
-        let (delimiter, span, content_cursor) = input.parse_any_delimiter()?;
-        Ok(Delimited {
-            delimiter,
-            span,
-            contents: TokenStream2::parse(&content_cursor)?,
-        })
-    }
-}
-
+#[allow(dead_code)]
 #[derive(Debug)]
 struct MacroRules2 {
-    macro_name: Ident,
-    macro_def: Delimited,
+    name: Ident,
+    body: MacroRulesDef,
 }
 
 impl Parse for MacroRules2 {
-    fn parse(input: ParseStream) -> syn::Result<Self> {
-        let macro_name = input.parse()?;
-        let macro_def = input.parse()?;
+    fn parse(input: ParseStream) -> ParseResult<Self> {
+        let name: Ident = input.parse()?;
+        let x: MacroDelimiter = input.parse()?;
+        let body = contents.parse()?;
 
-        Ok(MacroRules2 {
-            macro_name,
-            macro_def,
-        })
+        Ok(MacroRules2 { name, body })
+    }
+}
+
+impl ToTokens for MacroRules2 {
+    fn to_tokens(&self, tokens: &mut TokenStream2) {
+        let Self { name, body } = self;
+        *tokens = quote! {
+            quote! {
+                macro_rules! #name {
+                    #body
+                }
+            };
+        }
     }
 }
 
@@ -47,13 +65,11 @@ impl Parse for MacroRules2 {
 pub fn macro_rules2(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     eprintln!("parsing macro input");
 
-    let MacroRules2 { macro_def, .. } = {
-        let result = parse_macro_input!(input as MacroRules2);
-        eprintln!("{:?}", &result);
-        result
-    };
+    let parsed_macro_rules2 = parse_macro_input!(input as MacroRules2);
+    eprintln!("{:?}", &parsed_macro_rules2);
 
-    let tokens = macro_def.contents;
+    let mut tokens = TokenStream2::new();
+    parsed_macro_rules2.to_tokens(&mut tokens);
 
     eprintln!("output tokens:\n{:#?}", tokens.to_string());
     tokens.into()
